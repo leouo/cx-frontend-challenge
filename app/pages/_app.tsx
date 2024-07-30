01c22bf04aa471bdff7a7d77bb070a39db892aba
Leonardo <leonardoccpmorais@gmail.com>
2024-07-30 11:24:13 -0300

    chore: add roboto as default font family

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from '@next/font/google'
 import BaseLayout from '@/components/BaseLayout'
 import { SearchProvider } from '../context/searchContext'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps: { initialContext, ...pageProps } }: AppProps) {
   return (
     <SearchProvider initialValue={{ ...initialContext }}>
       <BaseLayout>
+        <style jsx global>
+          {`
+            html {
+              font-family: ${roboto.style.fontFamily};
+            }
+          `}
+        </style>
         <Component {...pageProps} />
       </BaseLayout>
     </SearchProvider>
