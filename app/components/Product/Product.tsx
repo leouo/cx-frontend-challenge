bdb9ae93982eaee8113b4a0da4ff52518489a01e
Leonardo <leonardoccpmorais@gmail.com>
2024-07-29 23:52:15 -0300

    fix: add placeholder for products without price

--- app/components/Product/Product.tsx
+++ app/components/Product/Product.tsx
@@ -25,7 +25,7 @@ export const Product: FC<IProduct> = ({ title, thumbnail, installments, price, f
                 <img className={styles.product__thumbnail} src={thumbnail} alt={title} />
             </figure>
             <div className={styles.product__content}>
-                <data className={styles.product__price}>{formatPrice(price)}</data>
+                <data className={styles.product__price}>{price ? formatPrice(price) : 'Precio a convenir'}</data>
                 <h2 className={styles.product__title}>{title}</h2>
                 {installments ? <p className={styles.product__installments}>En {installments?.quantity} cuotas de {formatPrice(installments?.amount)}</p> : null}
             </div>
